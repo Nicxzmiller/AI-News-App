d649a994f506d20d7ac54e99e864318afc149884
Nicholas <nicxzmiller2@gmail.com>
2020-08-06 22:42:41 +0200

    activating alan

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey ='a1f30d45111bdd8591369249532d03362e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command }) => {
+                if(command === 'testCommand'){
+                    alert('This code was executed')
+                }
+            }
+        })
+    }, [])
+
     return(
         <div>
             <h1>Alan AI Application</h1>
